85acd147c0044619cc75b77a387e15daa5264a6d
Wayne <wguo6358@gmail.com>
2023-11-10 07:47:42 +0000

    finished login

--- image-collecting/src/App.tsx
+++ image-collecting/src/App.tsx
@@ -3,7 +3,6 @@ import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
 import Main from './pages/Main';
-import { createClient } from "@supabase/supabase-js";
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -24,29 +23,41 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+
+import { useState, useEffect } from 'react'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+
+
 setupIonicReact({
   rippleEffect: true,
   mode: 'ios',
 });
 
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
+const App: React.FC = () => {
+  // useEffect(()=>{
+  //   supabase.auth.signInWithOAuth({
+  //     provider: 'google',
+  //   })
+  // },[])
+  return (
+    <IonApp>
+      <IonReactRouter>
+        <IonRouterOutlet>
+          <Route exact path="/home">
+            <Home />
+          </Route>
+          <Route exact path="/">
+            <Redirect to="/home" />
+          </Route>
 
-        <Route exact path="/main">
-          <Main />
-        </Route>    
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
-);
+          <Route exact path="/main">
+            <Main />
+          </Route>
+        </IonRouterOutlet>
+      </IonReactRouter>
+    </IonApp>)
+};
 
 export default App;
--- image-collecting/src/pages/Home.tsx
+++ image-collecting/src/pages/Home.tsx
@@ -1,6 +1,9 @@
-import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonInput, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonIcon, IonInput, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
+import { logoGoogle } from 'ionicons/icons';
+import { createClient } from "@supabase/supabase-js";
+const supabase = createClient('https://vcjgznqnyqmvkrommoyb.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZjamd6bnFueXFtdmtyb21tb3liIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc4MzY5NzEsImV4cCI6MjAxMzQxMjk3MX0.brt6zUmcuPJxpgyj42j8Mhd80KfXtmwtz0j52QjeC4o')
 
 const Home: React.FC = () => {
   return (
@@ -23,31 +26,45 @@ const Home: React.FC = () => {
 
           <IonCardContent>
             <ul>
-            <li> You will be given a series of molecules, you will be provided with their name and 3D sturture.
-            </li>
-            <li>You will need to build this molecule and take 4-6 images of it at different angle.</li>
-            <li>The image should be taken on a pure dark backgroun.</li>
-            <li>
-              If there is a chair flip for the molecule, do the chair flip and tkae another 4-6 images at different angle.
-            </li>
-            <li>
-              For best experence, we recommond using a tablet or large screen phone
-            </li>
-            <li> <b>Make sure all atoms are visible in the photo, if this is not possible because occlusion exsits, make as much visable as possib;e.</b></li>
+              <li> You will be given a series of molecules, you will be provided with their name and 3D sturture.
+              </li>
+              <li>You will need to build this molecule and take 4-6 images of it at different angle.</li>
+              <li>The image should be taken on a pure dark backgroun.</li>
+              <li>
+                If there is a chair flip for the molecule, do the chair flip and tkae another 4-6 images at different angle.
+              </li>
+              <li>
+                For best experence, we recommond using a tablet or large screen phone
+              </li>
+              <li> <b>Make sure all atoms are visible in the photo, if this is not possible because occlusion exsits, make as much visable as possib;e.</b></li>
             </ul>
-            <IonInput id="id">Volunteer ID:</IonInput>
-            <IonButton expand="block" onClick={()=>{
+            {/* <IonInput id="id">Volunteer ID:</IonInput> */}
+            <IonButton expand="block" onClick={() => {
               //@ts-ignore
-             const id=document.getElementById("id").value;
-             if(id)
-             {
-              window.location.href = "/main"
-             }
-             else{
-              alert("Please enter volunteer ID")
-             }
-            }}>Start</IonButton>
-            </IonCardContent>
+              //  const id=document.getElementById("id").value;
+              //  if(id)
+              //  {
+              //   window.location.href = "/main"
+              //  }
+              //  else{
+              //   alert("Please enter volunteer ID")
+              //  }
+              supabase.auth.getUser().then(user => {
+                if (!user.data.user) {
+                  supabase.auth.signInWithOAuth({
+                    provider: 'google',
+                  })
+                }
+                else {
+                  window.location.href = "/main"
+                }
+              })
+
+            }}>
+
+              <IonIcon icon={logoGoogle} style={{ padding: "10px" }}></IonIcon>
+              Login With Google</IonButton>
+          </IonCardContent>
         </IonCard>
       </IonContent>
     </IonPage>
