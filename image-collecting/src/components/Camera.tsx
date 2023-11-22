a8d0c96592033f641a32767739b6057f52b1fe75
Wayne <wguo6358@gmail.com>
2023-11-22 00:09:08 +0000

    solved adding error

--- image-collecting/src/components/Camera.tsx
+++ image-collecting/src/components/Camera.tsx
@@ -8,18 +8,24 @@ const Cam = (prop: any) => {
     const webcamRef = React.useRef(null);
     var images = prop.images
     var setImages = prop.setImages
-    const capture = React.useCallback(
-        () => {
-            const imageSrc = webcamRef.current.getScreenshot();
-            images.push(imageSrc)
-            setImages([...images])
-        },
-        [webcamRef]
-    );
+    // const capture = React.useCallback(
+    //     () => {
+    //         const imageSrc = webcamRef.current.getScreenshot();
+    //         images.push(imageSrc)
+    //         setImages([...images]) //after understand what is use call back i got why when i delete and take picture again it comes back because this is cached 
+    //     },
+    //     [webcamRef, images]
+    // );
+    // fuck when i was debuging i said not possible turns out i was doing it on nestify
+    function capture(){
+        const imageSrc = webcamRef.current.getScreenshot();
+        console.log(images)
+        setImages([...images, imageSrc])
+    }
     // const [images, setImages] = useState([])
     return (
         <>
-            <Webcam style={{height: "50%"}} ref={webcamRef} id="cam" videoConstraints={{ "facingMode": "environment" }}></Webcam>
+            <Webcam style={{ height: "50%" }} ref={webcamRef} id="cam" videoConstraints={{ "facingMode": "environment" }}></Webcam>
 
             <IonFab horizontal="center" vertical="bottom" onClick={capture}><IonFabButton><IonIcon icon={camera}></IonIcon></IonFabButton></IonFab>
 
